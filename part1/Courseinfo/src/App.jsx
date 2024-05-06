aba0ac8394fbdb7dd285e1195694c4849c14fcb5
JoseSandoval09 <joseeesp.09@gmail.com>
2024-05-05 18:02:01 -0600

    1d finished

--- part1/Courseinfo/src/App.jsx
+++ part1/Courseinfo/src/App.jsx
@@ -1,82 +1,26 @@
-const Header = (props) => {
-  console.log(props)
-  return(
-    <h1>{props.course}</h1>
-  )
-}
-const Content= (props) => {
-  console.log(props)
-  return(
-    <div>
-      <Part1 part1={props.part1} exercises1={props.exercises1}/>
-      <Part2 part2={props.part2} exercises2={props.exercises2}/>
-      <Part3 part3={props.part3} exercises3={props.exercises3}/>
-    </div>
-  )
-}
+import { useState } from 'react'
+const Display = props => <div>{props.value}</div>
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
-const Part1 = (props) => {
-  return(
-    <div>
-      <h2>{props.part1}</h2>
-      <p>{props.exercises1}</p>
-    </div>
-  )
-}
+const App = (props) => {
+  const [value, setValue] = useState(10)
 
-const Part2 = (props) => {
-  return(
-    <div>
-      <h2>{props.part2}</h2>
-      <p>{props.exercises2}</p>
-    </div>
-  )
-}
-
-const Part3 = (props) => {
-  return(
-    <div>
-      <h2>{props.part3}</h2>
-      <p>{props.exercises3}</p>
-    </div>
-  )
-}
-
-const Total= (props) => {
-  return(
-    <div>
-      <p>Total of exercises are : {props.total}</p>
-    </div>
-  )
-}
-
-
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
   }
 
   return (
     <div>
-      <Header course={course.name}/>
-      <Content part1={course.parts[0].name} exercises1={course.parts[0].exercises} part2={course.parts[1].name} exercises2={course.parts[1].exercises} part3={course.parts[2].name} exercises3={course.parts[2].exercises}/>
-      <Total total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/>
+      {value}
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
-
 export default App
